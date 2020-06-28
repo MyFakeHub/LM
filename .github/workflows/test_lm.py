2271b50c26391110fbde85163ef3d4ac13ee6be9
MyFakeHub <37661327+MyFakeHub@users.noreply.github.com>
2020-06-28 14:45:55 +0300

    Update test_lm.py

--- .github/workflows/test_lm.py
+++ .github/workflows/test_lm.py
@@ -1,5 +1,6 @@
 import LM
 
-nn = LM.NN([1, 5, 1])
-nn.train_lm([[1], [2], [3]], [[1], [2], [3]])
-assert True
+def test_main():
+  nn = LM.NN([1, 5, 1])
+  nn.train_lm([[1], [2], [3]], [[1], [2], [3]])
+  assert True
